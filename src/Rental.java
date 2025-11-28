2b3a77279a4cd275bf8942351079938c742880cb
Paulo Henrique <howpaulo50@gmail.com>
2025-11-27 22:27:23 -0300

    Commit 10

--- src/Rental.java
+++ src/Rental.java
@@ -21,11 +21,8 @@ public class Rental {
         return _movie.getCharge(_daysRented);
     }
 
-    // Este ainda não foi movido (será no próximo passo)
+
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
