cff57a3df4fe60967c78c39604e0cf6314caec6e
Paulo Henrique <howpaulo50@gmail.com>
2025-11-27 22:39:47 -0300

    Commit 13

--- src/Price.java
+++ src/Price.java
@@ -1,25 +1,6 @@
 public abstract class Price {
-
     public abstract int getPriceCode();
 
-    // NOVO MÉTODO MOVIDO DE MOVIE PARA CÁ
-    public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    // MUDANÇA: Virou abstrato. O switch sumiu!
+    public abstract double getCharge(int daysRented);
 }
