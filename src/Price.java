f41c6130c827890f9a287db065ca73b139c2c05d
Paulo Henrique <howpaulo50@gmail.com>
2025-11-27 22:35:37 -0300

    Commit 12

--- src/Price.java
+++ src/Price.java
@@ -1,3 +1,25 @@
 public abstract class Price {
+
     public abstract int getPriceCode();
+
+    // NOVO MÉTODO MOVIDO DE MOVIE PARA CÁ
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
